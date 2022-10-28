6293ee6b2b8d83053fbf37155ed927dfe3ccb053
Eric-JongYoung <fgh1937@yahoo.com>
2022-10-28 10:06:59 +0900

    3

--- src/main/java/dao/CartDAO.java
+++ src/main/java/dao/CartDAO.java
@@ -14,63 +14,30 @@ public interface CartDAO {
 	 */
 	int selectToInsert(String modelName, String emailId) throws SQLException;
 	CartDTO overlapCheck(String emailId, String modelName) throws SQLException;
-	
-<<<<<<< HEAD
-	/**
-	 * ��ȸ�� - ��ٱ��Ͽ� ��ǰ �߰�(session)
-	 * 
-	 */
-	
-=======
->>>>>>> merge221027
-	
-	
+
+
+
 	/**
 	 * ȸ�� - ��ٱ��� ��ǰ ��ȸ
 	 * select * from basket where email_id=?
 	 */
 	List<CartDTO> select(String emailId) throws SQLException;
-<<<<<<< HEAD
-	
-	
-	
-	/**
-	 * ��ȸ�� - ��ٱ��� ��ǰ ��ȸ
-	 */
-	
-=======
 	CartDTO selectForGuest(String modelName) throws SQLException;
->>>>>>> merge221027
-	
-	
+
+
 	/**
 	 * ȸ�� - ��ٱ��� ��ǰ ����
 	 * delete basket where email_id=? and model_num=?
 	 */
 	int delete(String cartNum) throws SQLException;
-	
-	
-<<<<<<< HEAD
-	/**
-	 * ��ȸ�� - ��ٱ��� ��ǰ ����
-	 */
-	
-	
-=======
->>>>>>> merge221027
-	
+
+
+
 	/**
 	 * ȸ�� - ��ٱ��� ��ǰ ���� ����
 	 * update basket set model_count=? where cart_num=?
 	 */
 	int update(String cartNum, int modelCount) throws SQLException;
-	
-	
-<<<<<<< HEAD
-	
-	/**
-	 * ��ȸ�� - ��ٱ��� ��ǰ ���� ����
-	 */
-=======
->>>>>>> merge221027
+
+
 }
